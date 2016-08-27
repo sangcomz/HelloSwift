4bf167c752d0120562d07b3446fe551f76502dd9
정석원 <sangcomz@naver.com>
2016-08-27 19:24:45 +0900

    텍스트 필드를 추가했다.

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtFldName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnSend(_ sender: AnyObject) {
+        let txt = txtFldName.text!
+        if(!txt.isEmpty){
+            lblHello.text = "Hello, "+txt
+        }
+    }
 }
-
